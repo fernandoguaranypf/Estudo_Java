60e198de56c1dea147ec2bddecb6cb49819f252d
fernandoguaranypf <74359575+fernandoguaranypf@users.noreply.github.com>
2020-11-12 09:56:39 -0300

    Add files via upload

---
+++ Exemplo2.java
@@ -0,0 +1,18 @@
+
+public class Exemplo2 {
+
+	public static void main(String[] args) {
+		/*Aqui se inicia o exemplo de variavel que recebe valor*/
+		int  num;//Declara uma variavel chamada de num
+		
+		num = 100;//Atribui a num o valor de 100
+		
+		System.out.println("Este valor é num: " + num);
+		
+		num = num * 2;
+		
+		System.out.println("O valor de num * 2 é " + num);
+
+	}
+
+}
