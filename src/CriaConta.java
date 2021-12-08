fbf35a664ae75952f19dbd7ec61711631f986a23
Fernando <fernandoguarany@outlook.com>
2021-12-08 00:56:54 -0300

    Finalizando a aula 2 da Alura

--- src/CriaConta.java
+++ src/CriaConta.java
@@ -5,6 +5,18 @@ public class CriaConta {
 		Conta primeiraConta = new Conta();
 		primeiraConta.saldo = 200;
 		System.out.println("O saldo da minha conta é " + primeiraConta.saldo);
+		
+		primeiraConta.saldo += 100;
+		System.out.println("O saldo da minha conta é " + primeiraConta.saldo);
+		
+		Conta segundaConta = new Conta();
+		segundaConta.saldo += 50;
+		
+		System.out.println("O saldo da minha conta é " + primeiraConta.saldo);
+		System.out.println("O saldo da minha conta é " + segundaConta.saldo);
+		
+		System.out.println(primeiraConta.agencia);
+		System.out.println(primeiraConta.numero);
 
 	}
 
